cc890af52f6e88fd8c146464a3830432d48fa7c5
jkdasari31 <jayakrishnardasari@gmail.com>
2024-02-12 10:08:45 +0530

    Terraform Basic files

--- EC2_Instance/ec2.tf
+++ EC2_Instance/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" {
+resource "aws_instance" "Instance01" {
   ami           = "ami-0f3c7d07486cad139" #devops-practice
   instance_type = "t2.micro"
   #vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
