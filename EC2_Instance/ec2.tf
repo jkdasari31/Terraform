4711bb7034737b3f25e3fa360b8821a2c3567b9b
jkdasari31 <jayakrishnardasari@gmail.com>
2024-02-12 10:28:58 +0530

    Terraform Basic files added subnet ID

--- EC2_Instance/ec2.tf
+++ EC2_Instance/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "Instance01" {
   ami           = "ami-0f3c7d07486cad139" #devops-practice
   instance_type = "t2.micro"
+  subnet_id = "subnet-0feda0d67eec4efb0"
   #vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
 
   tags = {
