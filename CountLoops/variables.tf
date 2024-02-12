1edeff35ca28fc4a3bdf7ed788d1896232b33e58
jkdasari31 <jayakrishnardasari@gmail.com>
2024-02-12 13:35:58 +0530

    Roboshop Instance Creation terraform script

---
+++ CountLoops/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+  type = list
+  default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z052408615HOJSVQ9OAX5"
+}
+
+variable "domain_name" {
+  default = "njkdr.online"
+}
