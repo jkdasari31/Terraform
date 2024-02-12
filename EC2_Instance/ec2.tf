7f8fc0e2d2d498c45632a3b822c8394ec2a28824
jkdasari31 <jayakrishnardasari@gmail.com>
2024-02-12 10:41:06 +0530

    Terraform Basic files

--- EC2_Instance/ec2.tf
+++ EC2_Instance/ec2.tf
@@ -1,10 +1,37 @@
 resource "aws_instance" "Instance01" {
   ami           = "ami-0f3c7d07486cad139" #devops-practice
   instance_type = "t2.micro"
-  subnet_id = "subnet-0feda0d67eec4efb0"
+  #subnet_id = "subnet-0feda0d67eec4efb0"
   #vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
 
   tags = {
     Name = "HelloTerraform"
   }
+}
+
+resource "aws_security_group" "roboshop-allowall" { #this is terraform name, for terraform reference only
+    name        = var.sg-name # this is for AWS
+    description = var.sg-description
+    #vpc_id      = aws_vpc.main.id
+
+    ingress {
+        description      = "Allow All ports"
+        from_port        = var.inbound-from-port # 0 means all ports
+        to_port          = 0 
+        protocol         = "tcp"
+        cidr_blocks      = var.cidr_blocks
+        #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        #ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "Roboshop-AllowAll"
+    }
 }
